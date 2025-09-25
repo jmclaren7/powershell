8dbf5ca3fc82e6c6a0fe006b7cfdae82f4b025a6
John McLaren <john@johnscs.com>
2025-09-25 12:46:56 -0400

    rename

---
+++ Stop-ExchangeServices.ps1
@@ -0,0 +1,2 @@
+$services = get-wmiobject win32_service | ? {$_.displayname -like "Microsoft Exchange*" -and $_.StartMode -eq "Auto"}
+foreach ($service in $services) {Start-Service $service.name -Force}
